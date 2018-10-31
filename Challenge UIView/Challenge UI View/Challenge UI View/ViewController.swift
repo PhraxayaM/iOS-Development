bc86c125e9a92d3f85dd032e0693540ce6d9b5b9
Matthew Phraxayavong <mphraxa@gmail.com>
2018-10-30 22:06:51 -0700

    first commit

---
+++ Challenge UIView/Challenge UI View/Challenge UI View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge UI View
+//
+//  Created by Matthew Phraxayavong on 10/30/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
